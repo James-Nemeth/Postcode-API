2c26aa752badbd620c540560357339e1e2f08b36
JamesImreNemeth <jamesnemeth1999@gmail.com>
2025-04-28 11:23:27 +1000

    first commit

---
+++ src/main/java/nology/io/postcode_api/PostcodeApiApplication.java
@@ -0,0 +1,13 @@
+package nology.io.postcode_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeApiApplication.class, args);
+	}
+
+}
